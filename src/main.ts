c2c8682f59db75859a269c8a39070c9f0e7e073e
fxy <f1655713437@gmail.com>
2026-09-05 13:52:51 +0800

    deploy: R7 几何重叠+伴热管白带修复

--- src/main.ts
+++
@@ -1,8 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-
-const app = createApp(App)
-app.config.errorHandler = (err, _inst, info) => {
-  console.error('[APP ERROR]', info, err)
-}
-app.mount('#app')
